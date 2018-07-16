e276776a4cffa5fc3fa5e7e799c42907208c570d
kirill <ger.kirill@gmail.com>
2018-07-16 14:29:03 +0300

    adds PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    // First application
+    {
+      name      : 'teahub',
+      script    : 'index.ts',
+      interpreter: 'node_modules/.bin/ts-node',
+      env: {
+        TZ: 'Europe/Kiev'
+      }
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : 'moydomen.com',
+      ref  : 'origin/master',
+      repo : 'git@github.com:gerkirill/teahub-welcome-bot.git',
+      path : '/home/ubuntu/teahub-welcome-bot-dev',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }/*,
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }*/
+  }
+};
